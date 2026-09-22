afa6911f4993ad3ea3272702771bdda4d5cadae0
Diptesh8 <shiva14420065@gmail.com>
2026-09-22 15:21:33 +0530

    added home and playground button section usiging javascript

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,8 @@
 function play(){
-    console.log('play start now');
+    // step1:=press thebutton and hide home section
+    const homeScreen = document.getElementById('home-screen');
+    homeScreen.classList.add('hidden')
+    // step2:= show the alpha playground
+    const alphaPlayGround =document.getElementById('playground')
+    alphaPlayGround.classList.remove('hidden');
 }
